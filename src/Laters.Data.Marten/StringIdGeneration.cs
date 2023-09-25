d42e7a9381a8ecbc7ffd6890a8c6c6c8fb1be17e
David Rundle <dbones@users.noreply.github.com>
2023-09-25 20:59:04 +0100

    brought back the disposable

---
+++ src/Laters.Data.Marten/StringIdGeneration.cs
@@ -0,0 +1,24 @@
+﻿namespace Laters.Data.Marten;
+
+using global::Marten.Schema;
+using global::Marten.Schema.Identity;
+using JasperFx.CodeGeneration;
+using JasperFx.CodeGeneration.Frames;
+using JasperFx.Core.Reflection;
+
+public class StringIdGeneration : IIdGeneration
+{
+    public void GenerateCode(GeneratedMethod method, DocumentMapping mapping)
+    {
+        Use use = new Use(mapping.DocumentType);
+        method.Frames.Code(
+            $"if ({{0}}.{mapping.IdMember.Name} == null)" +
+            $" _setter({{0}}, {typeof(Convert).FullNameInCode()}" +
+            $".ToBase64String({typeof(Guid).FullNameInCode()}.NewGuid().ToByteArray()).Replace(\"/\", \"_\").Replace(\"+\", \"-\").Substring(0, 22));",
+            use);
+        method.Frames.Code("return {0}." + mapping.IdMember.Name + ";", use);
+    }
+
+    public IEnumerable<Type> KeyTypes { get; } = new[] { typeof(string) };
+    public bool RequiresSequences => false;
+}
