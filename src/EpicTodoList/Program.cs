aa46577044b5a7abd61a706ea7c73da9d6357d81
dbones <dbones@users.noreply.github.com>
2024-03-17 12:14:08 +0000

    fixes to the demo

--- src/EpicTodoList/Program.cs
+++ src/EpicTodoList/Program.cs
@@ -1,47 +1,46 @@
+using System.ComponentModel.DataAnnotations;
 using Laters;
 using Laters.AspNet;
 using Laters.ClientProcessing;
 using Laters.Data.Marten;
 using Laters.Minimal.Application;
 using Marten;
+using Microsoft.EntityFrameworkCore;
 using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureServices((context, collection) =>
+//lets setup the database
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddMarten(config =>
 {
-    //lets setup the database
-    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-    collection.AddMarten(config =>
+    //read this from config.. but for now...
+    var connectionString = "host=postgres;database=laters;password=ABC123!!;username=application";
+    config.Connection(connectionString);
+    config.AutoCreateSchemaObjects = AutoCreate.All;
+    config.DatabaseSchemaName = "todoapp";
+
+    config.CreateDatabasesForTenants(tenant =>
     {
-        //read this from config.. but for now...
-        var connectionString = "host=localhost;database=laters;password=ABC123!!;username=application";
-        config.Connection(connectionString);
-        config.AutoCreateSchemaObjects = AutoCreate.All;
-        config.DatabaseSchemaName = "todoapp";
-    
-        config.CreateDatabasesForTenants(tenant =>
-        {
-            tenant.MaintenanceDatabase(connectionString);
-            tenant
-                .ForTenant()
-                .CheckAgainstPgDatabase()
-                .WithOwner("admin")
-                .WithEncoding("UTF-8")
-                .ConnectionLimit(-1)
-                .OnDatabaseCreated(_ => { });
-        });
-    
-        //Setup Laters State!  
-        config.Schema.Include<LatersRegistry>();
-    
+        tenant.MaintenanceDatabase(connectionString);
+        tenant
+            .ForTenant()
+            .CheckAgainstPgDatabase()
+            .WithOwner("admin")
+            .WithEncoding("UTF-8")
+            .ConnectionLimit(-1)
+            .OnDatabaseCreated(_ => { });
     });
-    
-    //quick workaround, you can also the SessionFactory
-    collection.AddScoped<IDocumentSession>(services =>
-        services.GetRequiredService<IDocumentStore>().DirtyTrackedSession());
+
+    //Setup Laters State!
+    config.Schema.Include<LatersRegistry>();
+
 });
 
+//quick workaround, you can also the SessionFactory
+builder.Services.AddScoped<IDocumentSession>(services =>
+    services.GetRequiredService<IDocumentStore>().DirtyTrackedSession());
 
 builder.WebHost.ConfigureLaters((context, setup) =>
 {
@@ -54,49 +53,47 @@ var app = builder.Build();
 //setup laters with aspnet (quick setup for demo)
 app.UseLaters(commit: CommitStrategy.SupplyMiddleware);
 
-app.MapGet("/todoitems/{id}", async (int id, IQuerySession session) =>
+//minimal http
+app.MapGet("/todo-items", (IQuerySession session) =>
+{
+    var items = session.Query<TodoItem>();
+    return Task.FromResult(items);
+});
+
+app.MapGet("/todo-items/{id}", async (int id, IQuerySession session) =>
     await session.LoadAsync<TodoItem>(id)
         is { } todo
         ? Results.Ok(todo)
         : Results.NotFound());
 
-app.MapPost("/todoitms", (TodoItem item, IDocumentSession session) =>
+app.MapPost("/todo-items", (TodoItem item, IDocumentSession session) =>
 {
     session.Store(item);
-    return Results.Created($"/todoitems/{item.Id}", item);
+    return Results.Created($"/todo-items/{item.Id}", item);
 });
 
-app.MapPut("/todoitems/{id}", async (
-    int id, 
-    TodoItem updateItem, 
+app.MapPut("/todo-items/{id}", async (
+    int id,
+    TodoItem updateItem,
     IDocumentSession session,
     ISchedule schedule) =>
 {
     var item = await session.LoadAsync<TodoItem>(id);
     if (item is null) return Results.NotFound();
 
-    var shouldRemoveJob = item.RemoveJobId is not null && !updateItem.CompletedDate.HasValue;
-    var shouldAddJob = !updateItem.CompletedDate.HasValue;
-    
     item.Details = updateItem.Details;
     item.CompletedDate = updateItem.CompletedDate;
 
-    if (shouldRemoveJob)
-    {
-        schedule.ForgetAboutIt<RemoveOldItem>(item.RemoveJobId!);
-    }
-
-    if (shouldAddJob)
+    if (item.CompletedDate.HasValue)
     {
         var removeDate = item.CompletedDate!.Value.AddDays(1);
-        var jobId = schedule.ForLater(new RemoveOldItem { Id = item.Id }, removeDate);
-        item.RemoveJobId = jobId;
+        schedule.ForLater(new RemoveOldItem { Id = item.Id }, removeDate);
     }
-    
+
     return Results.NoContent();
 });
 
-
+//minimal laters
 app.MapHandler<RemoveOldItem>(async (JobContext<RemoveOldItem> ctx, IDocumentSession session) =>
 {
     var itemId = ctx.Payload!.Id;
@@ -109,8 +106,12 @@ app.Run();
 //model
 public class TodoItem
 {
-    public string Id { get; set; }
-    public string Details { get; set; }
+    [Required(AllowEmptyStrings = false)]
+    public string Id { get; set; } = string.Empty;
+
+    [Required(AllowEmptyStrings = false)]
+    public string Details { get; set; } = string.Empty;
+
     public DateTime? CompletedDate { get; set; }
 
     public string? RemoveJobId { get; set; }
@@ -118,5 +119,5 @@ public class TodoItem
 
 public class RemoveOldItem
 {
-    public string Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 }
